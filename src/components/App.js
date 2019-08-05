24ffbf31d90347a85d83a4d2f2fffb9f8729bc5e
Huixiang Liu <forhjw@gmail.com>
2019-08-05 21:05:46 +0800

    add register page

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TopBar } from './TopBar';
+import { Main } from './Main';
+
+import '../styles/App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <TopBar/>
+        <Main/>
+      </div>
+  );
+}
+
+export default App;
+
